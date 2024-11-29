51f5fb41c1eb541eb2eefc3789dbdd96f62532db
Kanishq Prasad <98319337+kanishq-prasad@users.noreply.github.com>
2024-11-29 11:31:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ from env import env
 
 class Config:
     db_credentials = env['db_credentials']
-    SECRET_KEY = 'your-secret-key-here'
+    SECRET_KEY = env['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = "mysql://{}@127.0.0.1/chatapp".format(db_credentials)
